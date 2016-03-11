f35bb485fa7a031eb832ef61fc6f053f8bd116c8
Erik <erikdesjardins@users.noreply.github.com>
2016-03-10 23:15:06 -0500

    basic testing

--- test/test.js
+++ test/test.js
@@ -1,3 +1,28 @@
 import test from 'ava';
+import superagent from 'superagent';
+import superagentMock from 'superagent-mock';
 
-test.todo('everything');
+import deploy from '../index.js';
+
+test('missing fields', t => {
+	t.throws(
+		() => deploy({ clientSecret: 'q', refreshToken: 'q', id: 'q', zip: 'q' }),
+		'Missing required field: clientId'
+	);
+	t.throws(
+		() => deploy({ clientId: 'q', refreshToken: 'q', id: 'q', zip: 'q' }),
+		'Missing required field: clientSecret'
+	);
+	t.throws(
+		() => deploy({ clientId: 'q', clientSecret: 'q', id: 'q', zip: 'q' }),
+		'Missing required field: refreshToken'
+	);
+	t.throws(
+		() => deploy({ clientId: 'q', clientSecret: 'q', refreshToken: 'q', zip: 'q' }),
+		'Missing required field: id'
+	);
+	t.throws(
+		() => deploy({ clientId: 'q', clientSecret: 'q', refreshToken: 'q', id: 'q' }),
+		'Missing required field: zip'
+	);
+});
